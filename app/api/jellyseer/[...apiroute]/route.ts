db162f68c96481dacb693ce9a6c481ab3a07d612
Jae <71599416+Jheesbrough@users.noreply.github.com>
2025-03-15 17:02:16 +0000

    Fixed api route typing

--- app/api/jellyseer/[...apiroute]/route.ts
+++ app/api/jellyseer/[...apiroute]/route.ts
@@ -1,9 +1,9 @@
 import { sendAxiosJellyRequest } from '@/utils/axiosUtil';
-import type { NextApiRequest } from 'next';
+import { APIresponse } from '@/utils/types';
 import { headers } from 'next/headers';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-async function getBaseURLAndApiKey(request: NextApiRequest) {
+async function getBaseURLAndApiKey(request: NextRequest): Promise<{ baseURL: string, apiKey: string, searchParams: URLSearchParams }> {
   if (!request.url) { throw new Error('Invalid request URL'); }
   const { searchParams } = new URL(request.url);
   const baseURL = searchParams.get('baseurl');
@@ -27,7 +27,7 @@ async function getBaseURLAndApiKey(request: NextApiRequest) {
 **/
 const validRoutesRegex = /^(media|request|settings\/about|media\/[a-f0-9]{32}\/file|media\/[a-f0-9]{32})$/;
 
-export async function GET(request: NextApiRequest, { params }: { params: Promise<{ apiroute: string[] }> }) {
+export async function GET(request: NextRequest, { params }: { params: Promise<{ apiroute: string[] }> }): Promise<NextResponse> {
   const apiroute = (await params).apiroute.join('/');
 
   if (!validRoutesRegex.test(apiroute)) {
@@ -40,7 +40,7 @@ export async function GET(request: NextApiRequest, { params }: { params: Promise
   return makeRequest('get', url, apiKey);
 }
 
-export async function DELETE(request: NextApiRequest, { params }: { params: Promise<{ apiroute: string[] }> }) {
+export async function DELETE(request: NextRequest, { params }: { params: Promise<{ apiroute: string[] }> }): Promise<NextResponse> {
   const apiroute = (await params).apiroute.join('/');
 
   if (!validRoutesRegex.test(apiroute) || apiroute === 'media' || apiroute === 'request') {
@@ -52,15 +52,15 @@ export async function DELETE(request: NextApiRequest, { params }: { params: Prom
   return makeRequest('delete', url, apiKey);
 }
 
-function validateHttpUrl(url: URL) {
+function validateHttpUrl(url: URL): void {
   if (!url.protocol.startsWith('http') && !url.protocol.startsWith('https')) {
     throw new Error('Invalid URL protocol');
   }
 }
 
-export async function makeRequest(method: 'get' | 'delete', url: URL, apiKey: string) {
+async function makeRequest(method: 'get' | 'delete', url: URL, apiKey: string): Promise<NextResponse> {
   validateHttpUrl(url);
 
-  const response = await sendAxiosJellyRequest(url, method, { 'X-Api-Key': apiKey });
+  const response: APIresponse = await sendAxiosJellyRequest(url, method, { 'X-Api-Key': apiKey });
   return NextResponse.json(response, { status: 200 });
 }
